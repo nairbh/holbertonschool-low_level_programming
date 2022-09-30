5702a32484a8ac04c6debeb96c335fd6d934ef46
root <houcinenair@proton.me>
2022-09-30 09:37:11 -0700

    task6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,4 +1,18 @@
 #!/bin/bash
- #include <stdio.h>
+#include <stdio.h>
 
-int main(void) { int a; long int b; long long int c; char d; float f;
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+
+int main(void)
+{
+		printf("Size of a char: %lu byte(s)\n", sizeof(char));
+			printf("Size of an int: %lu byte(s)\n", sizeof(int));
+				printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+					printf("Size of a long long int: %lu byte(s)\n", sizeof(double));
+						printf("Size of a float: %lu byte(s)\n", sizeof(float));
+							return (0);
+}
