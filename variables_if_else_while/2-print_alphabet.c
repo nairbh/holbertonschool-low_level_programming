12dd36324c4bba5fe4d4b4bef03f183570431d99
root <houcinenair@proton.me>
2022-10-03 04:21:42 -0700

    task2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{char letter;
+for(letter = 'a'; letter < = 'z'; letter++);
+putchar(letter);
+putchar('\n');
+return 0;
+}
