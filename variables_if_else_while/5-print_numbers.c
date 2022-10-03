2e8c3c455e414c41c1c26c230547926f9b2a583a
root <houcinenair@proton.me>
2022-10-03 14:31:46 -0700

    task5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+int main(void)
+{
+	int i;
+
+	printf("Print a comma-separated list of numbers from 1 to 10 :\n");
+	for (i = 0; i <= 10; i++)
+{
+	printf("%d", i);
+
+	if (i < 10)
+
+	printf("\n");
+}
+}
