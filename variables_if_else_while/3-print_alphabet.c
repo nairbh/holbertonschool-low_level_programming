e722017f8cfdd776dfa8ef8c1acaaf6ec8c011cc
root <houcinenair@proton.me>
2022-10-03 08:58:42 -0700

    task3

---
+++ variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
+	write(1, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 26);
+	write(1, "\n", 1);
+	return (0);
+}
