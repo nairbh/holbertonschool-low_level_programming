1f4025f22b630f434cd76eb3710f845d68fa3746
root <houcinenair@proton.me>
2022-10-04 00:47:06 -0700

    task6

---
+++ variables_if_else_while/.vimrc
@@ -0,0 +1,6 @@
+et tabstop=8 shiftwidth=8
+set autoindent
+set smartindent
+set cindent
+syntax enable
+set number
---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *ft_number - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{	int a = 0;
+	for (; a < 10 ; a++)
+	putchar ('0' + a);
+
+	putchar('\n');
+	return (0);
+}
