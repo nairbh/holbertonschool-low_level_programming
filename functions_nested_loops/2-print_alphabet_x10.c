b3b541791c3690fed015a3bab8b5816664b3eb78
root <houcinenair@proton.me>
2022-10-06 03:49:18 -0700

    task2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - function that prints 10 itimes the alphabet
+**/
+
+void print_alphabet_x10(void)
+{	int i;
+
+	i = 0;
+
+	while (i < 10)
+{
+	char letter;
+
+	letter = 'a';
+
+	while (letter <= 'z')
+{	_putchar(letter);
+
+	letter++;
+}
+_putchar('\n');
+															i++;
+															}															}
