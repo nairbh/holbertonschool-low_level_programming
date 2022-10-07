223d89d0b7c569d91434db1d1f787c9906b82977
root <houcinenair@proton.me>
2022-10-07 04:08:41 -0700

    task3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ *print_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n <= 9)
+{
+	_putchar(n + '0');
+	n++;
+}
+	_putchar('\n');
+}
