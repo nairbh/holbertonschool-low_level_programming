874810e8efb9ae0a5a5fa83b6a899dadd8d85681
root <houcinenair@proton.me>
2022-10-07 08:48:44 -0700

    task9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include "stdio.h"
+#include "unistd.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	
+	n = ;
+	while (n <= 100)
+
+	{
+		if ((n % 15) == 0)
+			printf("FizzBuzz");
+		else if ((n % 5) == 0)
+			printf("Buzz");
+		else if ((n % 3) ==0)
+			printf("Fizz");
+		else
+			printf("%d", n);
+		if (n < 100)
+			putchar(' ');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
