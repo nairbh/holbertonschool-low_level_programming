62a9b7f14116a357cd676b81b0c3c4ed13276d8e
root <houcinenair@proton.me>
2022-10-07 06:29:29 -0700

    task5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /*
-*rint_numbers - prints the numbers, from 0 to 9, followed by a new line
+*print_numbers - prints the numbers, from 0 to 9, followed by a new line
 *
 */
 void more_numbers(void)
