bf6d161b790dfabe382168b3354ab35b6cd0a363
root <houcinenair@proton.me>
2022-10-07 04:00:41 -0700

    task1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ *_isdigit - Write a function that checks for uppercase character.
+ *@c: number
+ *Return: Alway 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	return (1);
+	return (0);
+}
