da5dfecfa63375a585b8753a67200a771e75d3fb
root <houcinenair@proton.me>
2022-10-08 05:46:43 -0700

    task5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /*
- * print_numbers - prints the numbers, from 0 to 14, followed by a new line.
- * Return: Alway 0;
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
  */
 void more_numbers(void)
 {
@@ -11,10 +10,10 @@ void more_numbers(void)
 
 		for (n = 0; n <= 9; n++)
 {
-		for (i = 0; i <= 14; i++)
-			/**
-			 * * more_numbers - 10 times the numbers, from 0 to 14
-			 *  */
+	for (i = 0; i <= 14; i++)
+/**
+* more_numbers - 10 times the numbers, from 0 to 14
+*/
 {
 		if (i > 9)
 {		_putchar(i / 10 + '0');
