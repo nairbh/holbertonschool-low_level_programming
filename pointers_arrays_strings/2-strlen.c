cb4a53639fc5b372b1421b9e3c1b7667fd4b2801
root <houcinenair@proton.me>
2022-10-11 06:30:20 -0700

    task2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <string.h>
-/**
- * int _strlen Write a function that returns the length of a string.
+/* _strlen - returns the length of a string
  *
- *@string
+ * @s: input string.
+ * Return: length of a string.
  */
+
 int _strlen(char *s)
 {
 	int count;
