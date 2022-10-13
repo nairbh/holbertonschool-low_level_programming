066c15694dca63c86e78a91e058dca1773f16602
root <houcinenair@proton.me>
2022-10-13 02:16:48 -0700

    task5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/*
+ * _strlen - length of string
+ *  @s: pointer
+ *  Return: _strlen
+ */
+int _strlen(char *s)
+{
+	int strlen;
+
+	strlen = 0;
+	while (s[strlen] != '\0')
+{
+	strlen++;
+}
+	return (strlen);
+}
+
+void rev_string(char *s)
+{
+		int i;
+		int l;
+		int r;
+
+		l = _strlen(s);
+		r = l - 1;
+		i = 0;
+		while (i < l / 2)
+{
+		int temp;
+
+		temp = s[i];
+		s[i] = s[r];
+		s[r] = temp;
+		r--;
+		i++;
+}
+}
