e971e9fc79eed43a3487d829e4f7287f6578a866
root <houcinenair@proton.me>
2022-10-13 03:06:45 -0700

    task6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * 6. Half the lies they tell about me aren't true
+ * puts2 - Write a function that prints every other character of a string
+ * @str : string
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != 0)
+{
+	if (i % 2 == 0)
+	_putchar(str[i]);
+	i++;
+}
+	_putchar('\n');
+}
