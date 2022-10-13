6dd469ee32c691dffa735e476d1c7d83ffcd7843
root <houcinenair@proton.me>
2022-10-13 04:00:35 -0700

    task7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts_half - function that prints half of string
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int j;
+
+	j = 0;
+	i = 0;
+	while (*(str + i))
+	i++;
+	j = i / 2;
+	if (i % 2)
+	j += 1;
+	while (j < i)
+{
+	_putchar(*(str + j));
+	j++;
+}
+	_putchar('\n');
+}
