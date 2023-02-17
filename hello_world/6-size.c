26d84dc44d321b249e48aa5f26de5a9db3bce236
nairbrh <houcinenair@gmail.com>
2023-02-17 07:36:38 -0800

    task7

---
+++ hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+char az;
+int er;
+long int ty;
+long long int ui;
+float op;
+
+printf("Size of a char: %zu byte(s) \n", sizeof(az));
+
+printf("Size of a char: %zu byte(s) \n", sizeof(er));
+
+printf("Size of a char: %zu byte(s) \n", sizeof(ty));
+
+printf("Size of a char: %zu byte(s) \n", sizeof(ui));
+
+printf("Size of a char: %zu byte(s) \n", sizeof(op));
+
+return (0);
+}
