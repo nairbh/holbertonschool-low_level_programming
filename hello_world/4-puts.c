2542d98f32a8e0aedad24a04e4a005b44f63f3c8
nairbrh <houcinenair@gmail.com>
2023-02-17 05:58:59 -0800

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#!/bin/bash
-include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-puts("/"Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+int main(void) {
+char  array[]="/"Programming is like building a multilingual puzzle;
+puts(array);
 return (0);
 }
-
