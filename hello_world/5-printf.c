445f07784949aad79f8f7dad85bef6ce79ade5ab
nairbrh <houcinenair@gmail.com>
2023-02-17 07:23:13 -0800

    task5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
-
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char hi[] = "with proper grammar, but the outcome is a piece of art,";
 
-printf("%s", hi);
+printf("%s\n", hi);
 return (0);
 }
