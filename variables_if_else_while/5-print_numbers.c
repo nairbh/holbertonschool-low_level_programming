1907c53b9aa95b0d60daa529ba6ae982e9b49a26
nairbrh <houcinenair@gmail.com>
2023-02-20 08:04:05 -0800

    task

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -4,7 +4,6 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 
 {
@@ -13,7 +12,8 @@ int ch;
 for (ch = 0; ch <= 9; ch++)
 
 printf("%d", ch);
-}
-printf("\n");
+
+putchar('\n');
+
 return (0);
 }
