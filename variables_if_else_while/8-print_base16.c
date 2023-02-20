b1ce2dea18eb8caa76b20e37755dce8195b721d6
nairbrh <houcinenair@gmail.com>
2023-02-20 09:27:40 -0800

    task8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int i;
+	int ch;
+
+for (i = 0; i <= 16; i++)
+{
+if (i < 10)
+
+{
+ch = i + '0';
+}
+else
+{
+ch = i - 10 + 'a';
+}
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
