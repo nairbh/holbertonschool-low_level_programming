0f01b31b58695b6a64b3a24b9b581cddb449d893
nairbrh <houcinenair@gmail.com>
2023-02-20 10:13:05 -0800

    task8

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,19 +8,18 @@
 int main(void)
 
 {
-int a;
+	int a;
 
-for (a = 0; a <= 9; a++)
-{
-if (a < 9)
-{
-putchar(a + '0');
-putchar(',');
-putchar(' ');
-}
+	for (a = 0; a <= 9; a++)
+	{
 
-if (a == 9)
-putchar(a + '0');
-}
-return (0);
+		putchar(a + '0');
+		if (a != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
