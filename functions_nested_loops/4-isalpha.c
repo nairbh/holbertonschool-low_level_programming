caf9db88eafa261f66241a5ea971f6656b3b3bf2
nairbrh <houcinenair@gmail.com>
2023-02-21 06:53:31 -0800

    task4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+
+	return (0);
+}
