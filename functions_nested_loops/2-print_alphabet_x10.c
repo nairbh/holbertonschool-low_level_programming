626ebd3a8809c6b045c00cf8ef19ca82236b2f80
nairbrh <houcinenair@gmail.com>
2023-02-21 06:06:40 -0800

    task2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+
+
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	char chs;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		for (chs = 'a'; chs < 'z'; chs++)
+		{
+			_putchar(chs);
+		}
+
+		_putchar('\n');
+	}
+}
