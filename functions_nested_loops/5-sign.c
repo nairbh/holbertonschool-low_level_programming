a4befcce4880ce8b37aa544df360abbd65b8a405
nairbrh <houcinenair@gmail.com>
2023-02-21 08:17:21 -0800

    task5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -12,25 +12,25 @@ int print_sign(int n)
 { 
 	int i = 1;
 
-	{
-		if (n >= 0)
-	{
-	_putchar('+');
+	
+if (n >= 0)
+{
+	{_putchar('+');
 	_putchar('0');
 	}
 
 	if (n == 0)
-	{ 
+	{
 	i = 0;
 	_putchar('0');
-
 	}
+	
 	if (n < 0)
-	{ i = -1;
+	{
+	 i = -1;
 		_putchar('-');
 
 		return (i);
 	}
-	}
-
+	
 }
