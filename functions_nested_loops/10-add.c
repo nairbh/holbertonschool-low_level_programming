9d1e1cf61d4d1d72c4708d591d1e9fa589f99868
nairbrh <houcinenair@gmail.com>
2023-02-22 11:46:06 -0800

    task9

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,12 +2,11 @@
 #include "main.h"
 
 /**
-  * add - Adds two values
-  * @n1: The first number to be computed
-  * @n2: The second number to be computed
-  *
-  * Return: Integer value
-  */
+* add - Adds two values
+* @a: The first number to be computed
+* @b: The second number to be computed
+* Return: Integer value
+*/
 
 int add(int a, int b)
 {
