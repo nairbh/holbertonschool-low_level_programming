8471005bdd2696547572e66fe584ff786e9ac59b
nairbrh <houcinenair@gmail.com>
2023-02-23 05:38:59 -0800

    task5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,13 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 		int a;
+
 		for (a = 0; a <= 14; a++)
 		{
-			if ( a > 9)
-			{
+			if (a > 9)
+				{
 				putchar(a / 10 + '0');
-			}
+				}
 			putchar(a % 10 + '0');
 		}
 		putchar('\n');
