e50abb35c7c722d8811f07570288f708b6f067f6
nairbrh <houcinenair@gmail.com>
2023-02-23 06:03:41 -0800

    task6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+
+		putchar('0');
+		putchar('\n');
+	}
+
+	else
+	{
+		int j;
+
+		for (j = 0; j < n; j++)
+		{
+			putchar('-');
+		}
+			putchar('\n');
+
+	}
+}
