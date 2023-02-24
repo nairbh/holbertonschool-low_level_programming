227fea62d386301623a3bc6c5d57779d750c7f36
nairbrh <houcinenair@gmail.com>
2023-02-24 10:30:45 -0800

    task7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_diagonal(int n)
+
+{
+	if (n < 0)
+	{
+		putchar('\n')
+	}
+
+	int i, j;
+
+	for (i = 0; i < 1; i++)
+	{
+		for ( j = 0; j < 1; j++;)
+		{
+			putchar(' ');
+		}
+
+		putchar('\\');
+		putchar('\n');
+	}
+}
