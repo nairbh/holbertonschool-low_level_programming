9e874082e5a5c9e1568f81b219943d01ccd63670
nairbrh <houcinenair@gmail.com>
2023-02-24 12:05:38 -0800

    taask

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -19,12 +19,12 @@ int main(void)
 
 		else if (i % 3 == 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
 
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		}
 
 		else {
