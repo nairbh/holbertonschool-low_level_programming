5e029dba180ce712e42eeb3337d30813fd7daa24
nairbrh <houcinenair@gmail.com>
2023-02-24 11:15:32 -0800

    task11

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+
+	for (i = 1; i <= size; i++)
+	{	
+		for (j = 1; < size - i; j++)
+		{
+			putchar(' ');
+		}
+
+		for (j = 1; j <= i; j++)
+		{
+		putchar('#');
+		}
+	putchar('\n');
+	}
+}	
