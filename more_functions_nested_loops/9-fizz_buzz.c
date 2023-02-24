de9055c5a0b8dd63acdd17984f2fb5011ba8b61d
nairbrh <houcinenair@gmail.com>
2023-02-24 12:12:25 -0800

    task9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,29 +9,29 @@ int main(void)
 
 {
 
-	int i;
-	for (i = 1; i <= 100; i++)
+	int n;
+
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf(" FizzBuzz");
 		}
-
-		else if (i % 3 == 0)
+		else if (n % 3 == 0)
 		{
 			printf(" Fizz");
 		}
-
-		else if (i % 5 == 0)
+		else if (n % 5 == 0)
 		{
 			printf(" Buzz");
 		}
-
-		else {
-			printf("%d ", i);
+		else
+		{
+			printf(" %d", n);
 		}
-
-
 	}
+	printf("\n");
 	return (0);
 }
