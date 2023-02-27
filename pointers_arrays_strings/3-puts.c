1cbd0e9bc9dd143347fe0da56964aa202caf6371
nairbrh <houcinenair@gmail.com>
2023-02-27 01:18:58 -0800

    task3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+void _puts(char *str)
+
+{
+
+	char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+}
