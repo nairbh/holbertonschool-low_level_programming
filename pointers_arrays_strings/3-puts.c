cea97b633998f057539533c4da063e397b553206
nairbrh <houcinenair@gmail.com>
2023-02-27 01:40:43 -0800

    task3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -3,11 +3,8 @@
 
 void _puts(char *str)
 
-
 {
-	char *ch;
-	ch = NULL;	
-	ch = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
 
-	_puts(ch);
+	_puts(str);
 }
