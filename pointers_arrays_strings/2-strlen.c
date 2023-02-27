32f74ce852513dba40fe97950584013aeb8e5f91
nairbrh <houcinenair@gmail.com>
2023-02-27 01:13:24 -0800

    task2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+int _strlen(char *s)
+
+{
+	char *string;
+
+	int longueur;
+
+	string = "first strlen!";
+
+	longeur = _strlen(string);
+
+	printf("%d\", string);
+}
