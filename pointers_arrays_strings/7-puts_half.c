3fbb0c1fcf50b30d8c3b756d1be2eef3a9ebcc8d
nairbrh <houcinenair@gmail.com>
2023-02-28 00:47:04 -0800

    task7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -3,25 +3,30 @@
 #include <stdio.h>
 
 void puts_half(char *str)
-{
-    int len = 0;
-    char *p = str;
+{	
+	int len = 0;
+	char *p = str;
 	int i;
 	int mid;
-    while (*p != '\0') {
-        len++;
-        p++;
-    }
-   
-   mid = len / 2;
-    if (len % 2 != 0) {
-        mid++;
-    }
+	
+	while (*p != '\0') 
 
-    for (i = mid; i < len; i++) {
-        putchar(*(str + i));
-    }
+{	
+		len++;
+		p++;
+}		
+		
+		mid = len / 2;
+		
+		if (len % 2 != 0) 
+		{
+			mid++;
+		}	
+	for (i = mid; i < len; i++)
+	{
+		putchar(*(str + i));
+	}		
 
-    putchar('\n');
+	putchar('\n');
 }
 
