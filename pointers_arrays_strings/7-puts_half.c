ad80ea8c4412d675a78e6bd607c88defc198c5a5
nairbrh <houcinenair@gmail.com>
2023-02-28 00:12:24 -0800

    task7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
 #include "main.h"
 #include <string.h>
-void puts_half(char *str)
+
+void puts_half(char *str) 
+
 {
+    int length = strlen(str);
+    int half;
+	int n;
+    if (length % 2 == 0) {
+        half = length / 2;
+    } else {
+        half = (length - 1) / 2;
+    }
 
-	int loong;
-	int half;
-
- loong = strlen(str);
-
-	if (loong % 2 == 0 != '\0')
-{
-	{
-		half = loong / 2;
-	}
-
-	else 
-	{
-		half = loong -1 / 2;
-	}
-		{
-			half = loong -1 / 2;
-		}
+    for (int n = half; n < length; n++) {
+        printf("%c", str[n]);
+    }
+    putchar('\n');
 }
 
-	for (i = half; i < loong; i++)
-	{
-		printf("%c",str[i]);
-	}
-	putchar('\n');
-}
