415893c6bbfecfb45167e6b9409f368ab472d18c
nairbrh <houcinenair@gmail.com>
2023-03-01 05:49:35 -0800

    task5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -3,7 +3,7 @@
 /**
 *_strlen - length of string
 * @s: pointer
-* Return: _strlen
+* Return: no return
 */
 
 void rev_string(char *s)
@@ -14,12 +14,12 @@ void rev_string(char *s)
 	while (s[len] != '\0')
 		len++;
 
-	for(i = 0; i < len/2; i++)
+	for(i = 0; i < len / 2; i++)
 	{
 		char tmp = s[i];
 
-		s[i] = s[len - i -1];
+		s[i] = s[len - i - 1];
 
-		s[len -i -1] = tmp;
+		s[len -i - 1] = tmp;
 	}
 }
