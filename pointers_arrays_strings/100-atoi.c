e67fc3577bcb8a76c679463d88492fd34a28941c
nairbrh <houcinenair@gmail.com>
2023-03-01 06:32:30 -0800

    task10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,7 +2,11 @@
 #include "main.h"
 #include <string.h>
 #include <stdlib.h>
-
+/**
+ * _isdigit - Check for digits (0 through 9)
+ * @c: int type param
+ * Return: 1 if digit, else 0
+ */
 int _isdigit(int c)
 {
 	if (c > 47 && c < 58)
