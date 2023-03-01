abea7594614032a95d10e62bdd94703670f09691
nairbrh <houcinenair@gmail.com>
2023-03-01 06:55:55 -0800

    task3

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,7 @@
 /**
  * string_toupper - changes all lowercase letters of a string
  * to uppercase
- * @s: input string.
+ * @str: input string.
  * Return: the pointer to dest.
  */
 
@@ -11,12 +11,11 @@ char *string_toupper(char *str)
 {
 	int count = 0;
 
-	while (*(str + count) != '\0')
+	while (str[count])
 	{
-		if ((*(str + count) >= 97) && (*(str + count) <= 122))
-		*(str + count) =- 32;
-		count++;
+		if (str[count] >= 'a' && str[count] <= 'z')
+		str[count] -= 32;
+	i++;
 	}
-
-	return (str);
+return (str);
 }
