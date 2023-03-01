df607d61963ce03265dcd6ee61dab233bf9cbff9
nairbrh <houcinenair@gmail.com>
2023-03-01 05:47:22 -0800

    task4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* _strlen - length of string;
-* @s : pointer
-* Return: _strlen
-*/
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: input string.
+ * Return: no return.
+ */
 void print_rev(char *s)
 
-{	
+{
 	int a = 0;
 
 	while (*(s + a) != '\0')
@@ -15,7 +15,7 @@ void print_rev(char *s)
 	{
 	int b;
 
-	for (b = a -1; b >= 0; b--)
+	for (b = a - 1; b >= 0; b--)
 
 		printf("%c", *(s + b));
 	}
