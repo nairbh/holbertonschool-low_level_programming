27ee7dbddf607af42727479d7679cf715bed36c5
nairbrh <houcinenair@gmail.com>
2023-03-01 06:30:17 -0800

    task10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,18 +3,30 @@
 #include <string.h>
 #include <stdlib.h>
 
-int _atoi(char *s) {
-    int sign = 1, num = 0;
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+		return (1);
 
-    while (*s && (*s < '0' || *s > '9')) {
-        if (*s++ == '-') {
-            sign = -1;
-        }
-    }
+	return (0);
+}
+/**
+ * _atoi - convert a string to an integer.
+ * @s: string
+ * Return: integer to be convert
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
 
-    while (*s >= '0' && *s <= '9') {
-        num = num * 10 + (*s++ - '0');
-    }
-
-    return num * sign;
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+	return (num * sign);
 }
