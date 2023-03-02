dd1fc897b2e0348c2827ba19cd35c8e9b71eb61b
nairbrh <houcinenair@gmail.com>
2023-03-02 06:47:26 -0800

    task3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count;
+
+
+	for (count = 0; s[count] && strspn(accept, s[count], count++)
+		{
+			return (count);
+		
+		}
+
+
+
+}
+
+
+
