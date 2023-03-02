19dc8ed7e4f8ef861f0b91692877a33423ccf363
nairbrh <houcinenair@gmail.com>
2023-03-02 04:05:02 -0800

    task1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,15 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
+	char *p_dest = dest;
+
+	const char *p_src = src;
+
 	for (i = 0; i < n; i++)
 
 	{
-		strncpy(dest, src, i == '\0');
+		p_desti[i] = p_src[i];
 	}
 
 	return (dest);
 }
-
