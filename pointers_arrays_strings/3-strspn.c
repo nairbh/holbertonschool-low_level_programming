e245b68b08060296d64430048789ff9218b63831
nairbrh <houcinenair@gmail.com>
2023-03-02 15:23:13 -0800

    task3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -5,25 +5,27 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i = 0;
-	 unsigned int j;
+	unsigned int j;
 
-	 while (s[i] != '\0')
+	while (s[i] != '\0')
 
-	 {	j = 0;
-		 while (accept[j] != '\0')
-		 {
-			 if (s[i] == accept[j])
-			 {
-				 break;
-			 }
-			 j++;
-		 }
+	{	j = 0;
 
-		 if (accept[j] == '\0')
-		 {
-			 break;
-		 }
-		 i++;
-	 }
-	 return (i);
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+					j++;
+		}
+
+		if (accept[j] == '\0')
+		{
+
+			break;
+		}
+			i++;
+	}
+	return (i);
 }
