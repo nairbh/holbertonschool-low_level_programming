c679efd30ef7467c79769809c40d05e32200de31
nairbrh <houcinenair@gmail.com>
2023-03-02 01:16:17 -0800

    task2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -18,12 +18,13 @@ char *_strncpy(char *dest, const char *src, int n)
 	dest[i] = src[i];
 	}
 
-	for (;i < n; i++) 
+	for (; i < n; i++)
 	{
 	dest[i] = '\0';
 	}
 
-    	return (dest);
+
+	return (dest);
 }
 
 
