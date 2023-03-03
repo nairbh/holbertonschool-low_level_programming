4e22116bb0678d51ed8066afef38f29690589050
nairbrh <houcinenair@gmail.com>
2023-03-03 06:37:21 -0800

    task5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include <string.h>
+#include "main.h"
+
+
+char *_strstr(char *haystack, char *needle)
+
+{
+
+	if (*needle == '\0')
+	{
+		return (*haystack)
+	}
+
+	while (*haystack != '\0')
+
+	{
+		char *h = haystack;
+		char *n = needle;
+		
+		while (*h == *n && *n != '\0')
+		{
+			*h++;
+			*n++;
+
+		}
+
+
+		if (*n == '\0')
+
+		{
+			return (haystack);
+		}
+
+		haystack++;
+	}
+
+	return (NULL);
+}
