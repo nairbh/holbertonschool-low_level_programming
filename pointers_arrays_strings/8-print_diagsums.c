2c109fa2f355eb46ef3fbe886c7d60db849420f4
nairbrh <houcinenair@gmail.com>
2023-03-05 10:46:45 -0800

    task7

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_diagsums(int *a, int size) {
+
+	int sum1 = 0, sum2 = 0;
+	int i;
+	for (i = 0; i < size; i++)
+
+	{
+
+		sum1 += a[i * size + i];
+
+		sum2 += a[i * size + size - i - 1];
+    	}
+
+
+		printf("%d, %d\n", sum1, sum2);
+}
+
