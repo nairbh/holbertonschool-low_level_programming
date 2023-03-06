8913e741c4d4716fd49f793f3a9b8ba4c6dbe070
nairbrh <houcinenair@gmail.com>
2023-03-06 13:14:19 -0800

    task5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 #include "main.h"
 
-int _sqrt_help(int n, int x, int y)
-
-	int _sqtr_recursion(int n)
-
+/**
+ * power_operation - returns the natural square root of a number.
+ * @n: input number.
+ * @c: iterator.
+ * Return: square root or -1.
+ */
+int power_operation(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
+ */
+int _sqrt_recursion(int n)
 {
 	if (n < 0)
-
-		return (- 1);
-
-	return (_sqrt_help(n, n, 1);
-
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
 }
-
-
-	if (x - x <= 0)
-	{
-	return (x);
-
-		x += y / 2;
-
-		y = n / x;
-
-		return(_sqrt_help(n, x, y));
-	}
