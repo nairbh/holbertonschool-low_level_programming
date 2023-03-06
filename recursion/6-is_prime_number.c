d08630f9f95d08ef3228431beeb8d09b21835db2
nairbrh <houcinenair@gmail.com>
2023-03-06 14:58:43 -0800

    taskt

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -21,7 +21,7 @@ int is_prime_number(int n)
 
 /**
  * is_prime_number - detects if an input number is a prime number.
- * @n: input number.
+ * @n: is_prime_find.
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
 int is_prime_find(int n, int i)
