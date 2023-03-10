1994044f9be8b1045697655a8ee8fbcf9d67a438
nairbrh <houcinenair@gmail.com>
2023-03-10 08:13:53 -0800

    task6

---
+++ argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+
+int get_number(int argc, char *argv[])
+
+
+{
+	int i, sum = 0;
+
+
+	for (i = 1; i < argc; i++)
+
+	{
+		int j;
+		for (j = 0; j < strlen(argv[i]); j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n",sum);
+	return (0);
+}
+
+
+int main(int argc, char *argv[])
+
+{
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+
+	else {
+		return (get_number(argc, argv));
+	}
+	return (0);
+}
