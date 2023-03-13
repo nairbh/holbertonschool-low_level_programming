41d93f5a13d3b48d0f8c2b123cc551a5bdf624ba
nairbrh <houcinenair@gmail.com>
2023-03-13 12:30:36 -0700

    task2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -6,27 +6,28 @@ extern size_t strnlen(const char *__string, size_t __maxlen);
 char *str_concat(char *s1, char *s2)
 
 {
-	
-	char *result;
+	size_t len1 = s1 ? strlen(s1) : 0;
+	size_t len2 = s2 ? strlen(s2) : 0;
 
-	if (!s1)
-	{
-		s1 = "";
-	}
-	if (!s2)
-	{
-		s2 = "";
-	}
-	size_t len1 = strlen(s1);
-	size_t len2 = strlen(s2);
-	result = malloc((len1 + len2 + 1) * sizeof(char));	
+	char *result = malloc(len1 + len2 + 1);
+
 	if (!result)
+
 	{
 		return (NULL);
 	}
 
-	memcpy(result, s1, len1);
-	memcpy(result + len1, s2, len2 + 1);
+	if (s1)
+	{
+		memcpy(result, s1, len1);
+	}
+
+	if (s2)
+	{
+		memcpy(result + len1, s2, len2 + 1);
+	}
+
+	result[len1 + len2] = '\0';
 
 	return (result);
 }
