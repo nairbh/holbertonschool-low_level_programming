e5647786a6e8e92632b440baa32bed818ea290ad
nairbrh <houcinenair@gmail.com>
2023-03-13 04:05:26 -0700

    task0

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+extern size_t strnlen(const char *__string, size_t __maxlen);
+char *_strdup(char *str)
+
+{
+	size_t len;
+	char *new_str;
+
+	if (str == NULL)
+
+		return (NULL);
+
+	len = strlen(str + 1);
+
+	new_str = malloc(len + 1);
+
+	if (new_str == NULL)
+
+		return (NULL);
+
+
+	memcpy(new_str, str, len + 1);
+
+	return (new_str);
+}
