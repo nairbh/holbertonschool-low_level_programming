3c359fdd45b28c4640d2316c934c38ac4f3b4a89
nairbrh <houcinenair@gmail.com>
2023-03-14 05:12:42 -0700

    task0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include "main.h"
 void *malloc_checked(unsigned int b)
-{
-    void *ptr = malloc(b);
-    if (ptr == NULL) 
-    {
-        exit(98);
-    }
-    return ptr;
+{	
+	void *ptr = malloc(b);
+	
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	
+	return ptr;
 }
