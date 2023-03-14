f072b74c3ecc0db574c0658c5edab4003423885e
nairbrh <houcinenair@gmail.com>
2023-03-14 01:25:10 -0700

    task3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+int **alloc_grid(int width, int height)
+
+{
+	int i;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	int **grid = malloc(height * sizeof(int *));
+
+	if (grid == 0)
+	{
+		return (NULL);
+	}
+
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = calloc(width, sizeof(int));
+
+		if (grid[i] == 0)
+		{ return (NULL);
+		}
+
+		while (--i >= 0)
+		{
+			free(grid[i]);
+		}
+		free(grid);
+		return (NULL);
+	}
+	return (grid);
+}
+
