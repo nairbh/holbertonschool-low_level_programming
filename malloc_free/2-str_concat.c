c2d2129e7d54b0f18c63b6fc12d1d730974e7589
nairbrh <houcinenair@gmail.com>
2023-03-15 07:29:07 -0700

    description commit

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,8 +9,8 @@
  *
  * Return: pointer of an array of chars
  */
-char *str_concat(char *s1, char *s2)
 extern size_t strnlen(const char *__string, size_t __maxlen);
+char *str_concat(char *s1, char *s2)
 {
 	size_t len1 = s1 ? strlen(s1) : 0;
 	size_t len2 = s2 ? strlen(s2) : 0;
