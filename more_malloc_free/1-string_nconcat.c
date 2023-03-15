231af8f971fce7f5b2ffc6f0698c32e360c664a2
nairbrh <houcinenair@gmail.com>
2023-03-15 05:41:34 -0700

    task1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+#include <ctype.h>
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+
+{
+	char *result;
+	size_t len1;
+	size_t len2;
+
+	if (s1 == NULL) return (NULL);
+	if (s2 == NULL) return (NULL);
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	if (n >= len2) n = len2;
+
+	result = malloc(len1 + n + 1);
+
+	if (result == NULL) return (NULL);
+
+	memcpy(result, s1, len1);
+	memcpy(result + len1, s2, n);
+
+	result[len1 + n] = '\0';
+
+	return (result);
+}
