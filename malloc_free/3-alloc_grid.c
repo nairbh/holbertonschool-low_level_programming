9098757ce7d6372b81dbc85b1c1ff8d678689456
nairbrh <houcinenair@gmail.com>
2023-03-15 06:54:27 -0700

    task3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -12,8 +12,7 @@
 int **alloc_grid(int width, int height)
 
 {
-	int i;
-	int j;
+	int i, j;
 	int **grid;
 
 	if (width <= 0 || height <= 0)
@@ -45,4 +44,5 @@ int **alloc_grid(int width, int height)
     return (NULL);
 }
 }
+return (grid);
 }
