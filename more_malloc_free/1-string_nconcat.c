baed614d4634af4a07bdd194552505af91892e98
nairbrh <houcinenair@gmail.com>
2023-03-15 05:43:21 -0700

    task1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -10,8 +10,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	size_t len1;
 	size_t len2;
 
-	if (s1 == NULL) return (NULL);
-	if (s2 == NULL) return (NULL);
+	if (s1 == NULL) s1 = "";
+	if (s2 == NULL) s2 = "";
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
