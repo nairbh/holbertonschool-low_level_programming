b6946e21195478d00f59d36adb11155a7c96b834
nairbrh <houcinenair@gmail.com>
2023-03-20 06:16:11 -0700

    task1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+* init_dog - function
+* @d: struct dog ptr
+* @name: char ptr
+* @age: float
+* @owner: char ptr
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+
+{
+	if (d)
+
+		return;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+
+}
