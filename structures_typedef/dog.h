ccd7aba389a69a1ebd486c15deb7cf6691bc0520
nairbrh <houcinenair@gmail.com>
2023-03-20 05:24:04 -0700

    task0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * struct dog - define new type
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
