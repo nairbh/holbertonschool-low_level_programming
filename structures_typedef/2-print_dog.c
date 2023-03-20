e01576b3bea44f1ff26f794f8e8bbe9c05581060
nairbrh <houcinenair@gmail.com>
2023-03-20 07:14:44 -0700

    task2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include "dog.h"
-
+/**
+ * print_dog - function
+ * @d: struct dog ptr
+ */
 void print_dog(struct dog *d)
 
 {
 	if (d == NULL)
 	{
-		printf("nil");
+		return;
+
+	printf("Name: (nil) %s\n", d->name != NULL ? d->name : (nil))
+	printf("Age: (nil) %1f\n", d->age != NULL ? d->age : (nil));
+	printf("Owner: (nil) %s\n", d->owner != NULL ? d->owner : (nill));
 	}
-	if (name == NULL)
-	{
-		printf("Name: (nil)");
-	}
-};
+}
