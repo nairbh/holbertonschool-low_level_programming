f8146d23f3abf25575c757b29c7c71f37bd9ed38
nairbrh <houcinenair@gmail.com>
2023-03-20 07:23:02 -0700

    task2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,5 +15,5 @@ void print_dog(struct dog *d)
 	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
 	printf("Age: %1f\n", d->age);
 	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
-	}
+
 }
