054fe9027b136b44fecd024b16b0b6466e8b279e
nairbrh <houcinenair@gmail.com>
2023-03-20 05:38:06 -0700

    task0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#ifndef DOG_H
+# define DOG_H
 /**
  * struct dog - define new type
  * @name: name
@@ -12,4 +13,12 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+typedef struct dog dog_t;
+void	init_dog(struct dog *d, char *name, float age, char *owner);
+void	print_dog(struct dog *d);
+dog_t	*new_dog(char *name, float age, char *owner);
+void	free_dog(dog_t *d);
+
+#endif /* DOG_H */
