5d55024991b41fbeef45c32005a4308322d7cd2f
nairbrh <houcinenair@gmail.com>
2023-03-21 03:40:11 -0700

    task0

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#ifndef FUUNCTION_H
-#define FUNCTION_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-#endif /* FUNCTION_H */
+#endif /* FUNCTION_POINTERS_H */
