7243a00b7d2786bcab5471d9c438de1e3f3a8f2c
nairbrh <houcinenair@gmail.com>
2023-03-24 12:44:45 -0700

    100-hello_world.asm

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,16 +1,17 @@
 section .data
-	msg db "Hello, World",5
-
+	msg db "Hello, World", 0Ah
 	section .text
-	global main
+	global _start
 
-main:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, 17
-	syscall
+_start:
+	mov edx, 13 ; length of the message
+	mov ecx, msg ; pointer to the message
+	mov ebx, 1 ; file descriptor for stdout
+	mov eax, 4 ; system call number for sys_write
 
-	mov rax, 60
-	mov rdi, 0
-	syscall
+int 0x80 ; make the system call
+
+; exit the program
+	mov eax, 1 ; system call number for sys_exit
+	xor ebx, ebx ; return value 0
+	int 0x80 ; make the system call
