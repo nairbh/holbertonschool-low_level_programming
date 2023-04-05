c380595fcf077b04a991796ca495539ce9d8b471
nairbrh <houcinenair@gmail.com>
2023-04-05 14:12:38 -0700

    fix error

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -3,7 +3,8 @@
 /*
 *premain - function that prints a message before the main function is
 */
-void attribute((constructor))premain()
+void premain() __attribute__ ((constructor));
+void premain()
 {
 printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
