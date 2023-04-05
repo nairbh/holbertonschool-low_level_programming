9bdfe1b15d7cc6d0e93346f9e6265a731f831a9e
nairbrh <houcinenair@gmail.com>
2023-04-05 14:16:55 -0700

    fixed error

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
-/*
-*premain - function that prints a message before the main function is
-*/
-void premain() __attribute__ ((constructor));
-void premain()
+/**
+ * premain - A function that is executed before the main function
+ * This function is used to print a message to the standard output
+ */
+void __attribute__ ((constructor)) premain(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
