b0443035bafbb3809038e0071052e7b29bc73455
nairbrh <houcinenair@gmail.com>
2023-04-07 12:36:31 -0700

    protected from double inclusion

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -7,3 +9,4 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+#endif /* MAIN_H */
