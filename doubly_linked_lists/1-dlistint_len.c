2dedd9aabaaf6002a425e04066de4101e2377614
nairbrh <houcinenair@gmail.com>
2023-04-06 21:57:16 -0700

    fix betty style

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
-/*
- * dlistint_len - returns the number of elements
-
-* @h: pointer to the head of the list
-
-* Return: the number of elements in the list
+/**
+* dlistint_len - returns the number of elements in
+* a double linked list
+*
+* @h: head of the list
+* Return: the number of nodes
 */
 size_t dlistint_len(const dlistint_t *h)
 {
--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,10 +1,12 @@
 #include "lists.h"
-/*
-* add_dnodeint - Adds a new node at the beginning.
-* @head: Double pointer to the head of the list.
-* @n: Integer value to be stored in the new node.
-* Return: Pointer to the new node.
-*/
+/**
+ * add_dnodeint - adds a new node at the beginning
+ * of a dlistint_t list
+ *
+ * @head: head of the list
+ * @n: value of the element
+ * Return: the address of the new element
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
