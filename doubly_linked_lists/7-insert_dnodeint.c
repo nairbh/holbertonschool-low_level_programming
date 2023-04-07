3d2bc6c8c9a80096be8a4a899f873b3f82769a1a
nairbrh <houcinenair@gmail.com>
2023-04-06 22:09:43 -0700

    fix error

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,6 +13,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *current = *h;
 	unsigned int i;
 
+	if (*h == NULL && idx != 0)
+		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 
 	if (!new)
