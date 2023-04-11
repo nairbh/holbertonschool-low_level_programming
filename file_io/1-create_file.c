4cc1ef579405a81c79a4a7ddaa1117d680354a65
nairbrh <houcinenair@gmail.com>
2023-04-11 03:54:33 -0700

    fix error

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * create_file - Creates a file with the given name and writes the given text
+ * create_file - Creates a file
  * content to it.
- *
- * @filename: A pointer to a string with the name of the file to be created.
- * @text_content: A pointer to a string with the content to be written to the
+ * @filename: A pointer to a string with
+ * @text_content: A pointer to a string
  * file. If NULL, an empty file will be created.
- *
  * Return: 1 on success, -1 on failure
  **/
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+
 	ssize_t len;
 
 	if (filename == NULL)
@@ -19,7 +18,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-
+	if (text_content == NULL)
+{
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
@@ -28,14 +28,22 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
+	close(fd);
+	return (1);
+}
 	len = write(fd, text_content, strlen(text_content));
-
 	if (len == -1)
 	{
 		close(fd);
 		return (-1);
 	}
-
+	close(fd);
+	return (1);
+	if (len == -1)
+	{
+		close(fd);
+		return (-1);
+	}
 	close(fd);
 	return (1);
 }
