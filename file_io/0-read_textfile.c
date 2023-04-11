894f7cd3e3e93def8b1ffb82f8be151b39a133b3
nairbrh <houcinenair@gmail.com>
2023-04-11 01:37:25 -0700

    read

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file
+ * @filename: pointer to the name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: the actual number of letters read and printed, or 0 upon failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	void *buffer;
+	ssize_t rd;
+
+	if (filename == NULL)
+
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+		if (fd == -1)
+		{
+			close(fd);
+			return (0);
+		}
+
+	buffer = malloc(letters);
+
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	 rd = read(fd, buffer, letters);
+
+	if (rd == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
+	if (write(STDOUT_FILENO, buffer, rd) == -1)
+	{
+		return (0);
+		free(buffer);
+	}
+	free(buffer);
+	close(fd);
+	return (rd);
+}
