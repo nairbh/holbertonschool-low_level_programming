09e7edc25e65a0320d3788f62d93895f528ce78f
nairbrh <houcinenair@gmail.com>
2023-04-12 20:50:19 -0700

    create file update

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,48 +1,31 @@
 #include "main.h"
 /**
- * create_file - Creates a file
- * content to it.
- * @filename: A pointer to a string with
- * @text_content: A pointer to a string
- * file. If NULL, an empty file will be created.
+ * create_file - creat new file
+ * @filename : name of new file
+ * @text_content: text inside new file
  * Return: 1 on success, -1 on failure
- **/
+ */
 int create_file(const char *filename, char *text_content)
-{
+{	
 	int fd;
-
-	ssize_t len;
+	int len = 0;
+	int wr;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-
-	if (text_content == NULL)
-{
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	
 	if (fd == -1)
-	{
-		close(fd);
 		return (-1);
-	}
-
-	close(fd);
-	return (1);
-}
-	len = write(fd, text_content, strlen(text_content));
-	if (len == -1)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
-	}
-	close(fd);
-	return (1);
-	if (len == -1)
-	{
-		close(fd);
-		return (-1);
+	while (text_content[len])
+		len++;
+	wr = write(fd, text_content, len);
+		if (wr == -1)
+		{	close(fd);
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
