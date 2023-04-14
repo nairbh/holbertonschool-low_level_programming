99521ab2905e6dd5f9d9802e3c6a676c15cbf516
nairbrh <houcinenair@gmail.com>
2023-04-14 05:00:55 -0700

    hash

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -20,8 +20,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 			free(temp_node->value);
 			temp_node->value = strdup(value);
-		if (temp_node->value == NULL)
-			return (0);
 			return (1);
 	}
 		temp_node = temp_node->next;
--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -6,7 +6,7 @@
  *
  * Return: Nothing!
  */
-void void hash_table_delete(hash_table_t *ht)
+void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *node, *tmp;
