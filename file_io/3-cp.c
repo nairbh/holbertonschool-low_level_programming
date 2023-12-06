a1b55e91aa1fac23174f8708febb02a293a55c49
nairbh <houcinenair@proton.me>
2023-12-06 20:38:03 +0100

    update

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,76 +8,60 @@
  * @argv: arguments
  * Return: int
 */
-#include "main.h"
-#include <stdio.h>
-#include <sys/stat.h>
-
-void check_arguments(int argc) {
-    if (argc != 3) {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
+void argc_error(int argc)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 }
-
-int open_source_file(const char *filename) {
-    int fd = open(filename, O_RDONLY);
-    if (!filename || fd == -1) {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-        exit(98);
-    }
-    return fd;
+int from_source(const char *filename)
+{
+		from_fd = open(argv[1], O_RDONLY);
+	if (!argv[1] || from_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	return (from_fd);
 }
-
-int open_destination_file(const char *filename) {
-    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (fd == -1) {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-        exit(99);
-    }
-    return fd;
-}
-
-void copy_content(int from_fd, int to_fd) {
-    char buffer[1024];
-    int read_bytes, written_bytes;
-
-    while ((read_bytes = read(from_fd, buffer, 1024)) > 0) {
-        written_bytes = write(to_fd, buffer, read_bytes);
-        if (written_bytes != read_bytes || written_bytes == -1) {
-            dprintf(STDERR_FILENO, "Error: Can't write to fd\n");
-            close(from_fd);
-            close(to_fd);
-            exit(99);
-        }
-    }
-    if (read_bytes == -1) {
-        dprintf(STDERR_FILENO, "Error: Can't read from fd\n");
-        close(from_fd);
-        close(to_fd);
-        exit(98);
-    }
-}
-
-void close_file(int fd) {
-    if (close(fd) == -1) {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-        exit(100);
-    }
-}
-
-int main(int argc, char **argv) {
-    mode_t oldmask = umask(0);
-    int from_fd, to_fd;
-
-    check_arguments(argc);
-    from_fd = open_source_file(argv[1]);
-    to_fd = open_destination_file(argv[2]);
-    umask(oldmask);
-
-    copy_content(from_fd, to_fd);
-
-    close_file(from_fd);
-    close_file(to_fd);
-
-    return 0;
+int main(int argc, char **argv)
+{
+	mode_t oldmask = umask(0);
+	int from_fd, to_fd, readed, writed;
+	char size[1024];
+	argc_error(argc);
+	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	umask(oldmask);
+	
+	while ((readed = read(from_fd, size, 1024)) > 0)
+	{
+		writed = write(to_fd, size, readed);
+		if (writed != readed || writed == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(from_fd);
+			close(to_fd);
+			exit(99);
+		}
+	}
+	if (readed == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(from_fd);
+		close(to_fd);
+		exit(98);
+	}
+	if (close(from_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_fd);
+		exit(100);
+	}
+	if (close(to_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_fd);
+		exit(100);
+	}
+	return (0);
 }
