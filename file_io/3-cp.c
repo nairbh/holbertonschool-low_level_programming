3fd098397ed9bb3ccf13a4a7b4217344452d8835
Houcine <113390396+nairbh@users.noreply.github.com>
2023-12-06 21:19:36 +0100

    Update 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,67 +1,58 @@
 #include "main.h"
-#include <stdio.h>
-#include <sys/stat.h>
 /**
  * main - entry point
  * @argc: numbers of arguments
  * @argv: arguments
  * Return: int
 */
-void argc_error(int argc)
-{
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+void argc_error(int argc) {
+    if (argc != 3) {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
 }
-int from_source(const char *filename)
-{
-	int from_fd = open(argv[1], O_RDONLY);
-	if (!argv[1] || from_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	return (from_fd);
+
+int from_source(const char *filename) {
+    int from_fd = open(filename, O_RDONLY);
+    if (!filename || from_fd == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+        exit(98);
+    }
+    return from_fd;
 }
-int main(int argc, char **argv)
-{
-	mode_t oldmask = umask(0);
-	int to_fd, readed, writed, from_fd;
-	char size[1024];
-	argc_error(argc);
-	from_fd = open_source_file(argv[1]);
-	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	umask(oldmask);
-	
-	while ((readed = read(from_fd, size, 1024)) > 0)
-	{
-		writed = write(to_fd, size, readed);
-		if (writed != readed || writed == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(from_fd);
-			close(to_fd);
-			exit(99);
-		}
-	}
-	if (readed == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(from_fd);
-		close(to_fd);
-		exit(98);
-	}
-	if (close(from_fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_fd);
-		exit(100);
-	}
-	if (close(to_fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_fd);
-		exit(100);
-	}
-	return (0);
+
+int main(int argc, char **argv) {
+    mode_t oldmask = umask(0);
+    int from_fd, to_fd, readed, writed;
+    char size[1024];
+
+    argc_error(argc);
+    from_fd = from_source(argv[1]);
+    to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    umask(oldmask);
+    
+    while ((readed = read(from_fd, size, 1024)) > 0) {
+        writed = write(to_fd, size, readed);
+        if (writed != readed || writed == -1) {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+            close(from_fd);
+            close(to_fd);
+            exit(99);
+        }
+    }
+    if (readed == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        close(from_fd);
+        close(to_fd);
+        exit(98);
+    }
+    if (close(from_fd) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_fd);
+        exit(100);
+    }
+    if (close(to_fd) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_fd);
+        exit(100);
+    }
+    return 0;
 }
