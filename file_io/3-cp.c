f7104cd44905e25892fb225f3b1eabbec143dc48
nairbh <houcinenair@proton.me>
2023-12-06 21:00:56 +0100

    update

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,10 +1,9 @@
 #include "main.h"
+
 /**
- * main - entry point
- * @argc: numbers of arguments
- * @argv: arguments
- * Return: int
-*/
+ * argc_error - checks if the number of arguments is correct
+ * @argc: number of arguments
+ */
 void argc_error(int argc)
 {
 	if (argc != 3)
@@ -13,25 +12,40 @@ void argc_error(int argc)
 		exit(97);
 	}
 }
+
+/**
+ * from_source - opens the source file
+ * @filename: name of the file to open
+ * Return: file descriptor of the opened file
+ */
 int from_source(const char *filename)
 {
-		from_fd = open(argv[1], O_RDONLY);
-	if (!argv[1] || from_fd == -1)
+	int from_fd = open(filename, O_RDONLY);
+	if (!filename || from_fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
 		exit(98);
 	}
-	return (from_fd);
+	return from_fd;
 }
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success, exit code on failure
+ */
 int main(int argc, char **argv)
 {
 	mode_t oldmask = umask(0);
 	int from_fd, to_fd, readed, writed;
 	char size[1024];
+
 	argc_error(argc);
+	from_fd = from_source(argv[1]);
 	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	umask(oldmask);
-	
+
 	while ((readed = read(from_fd, size, 1024)) > 0)
 	{
 		writed = write(to_fd, size, readed);
@@ -60,5 +74,5 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_fd);
 		exit(100);
 	}
-	return (0);
+	return 0;
 }
