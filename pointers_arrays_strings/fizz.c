df76030334ad4e392e522100902d84e881b6656c
root <root@sunny>
2024-03-01 12:51:11 +0100

    rm file swp

---
+++ pointers_arrays_strings/fizz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: This will print a text,
+ * replacing multiples of 3 by "Fizz",
+ * multiples of 5 by "Buzz" and multiples of both by "FizzBuzz"
+ * Return: 0 if success
+ */
+int main(void)
+{
+    int n;
+
+    for (n = 1; n <= 100; ++n)
+    {
+        if (n % 15 == 0)
+        {
+            printf("FizzBuzz");
+        }
+        else if (n % 5 == 0)
+        {
+            printf("Buzz");
+        }
+        else if (n % 3 == 0)
+        {
+            printf("Fizz");
+        }
+        else
+        {
+            printf("%d", n);
+        }
+        // Ajout d'un espace seulement si ce n'est pas le dernier nombre
+        if (n < 100)
+        {
+            printf(" ");
+        }
+    }
+    return 0;
+}
+
